633482d5146c3bff37738741ed034cb944afdc6b
Maxim Petsoliak <petsol.reserve228@gmail.com>
2025-12-25 18:35:36 +0200

    remove unused scripts

--- scripts/game/turn_manager.gd
+++ scripts/game/turn_manager.gd
@@ -11,6 +11,8 @@ var turn_sequense: Array[Enums.CharacterType] = [
 
 var current_turn: Enums.CharacterType = turn_sequense[0]
 
+signal turn_changed(turn)
+
 func end_turn():
 	var index = turn_sequense.find(current_turn)
 	current_turn = (index + 1) % turn_sequense.size() as Enums.CharacterType
--- scripts/utils/events.gd
+++ scripts/utils/events.gd
@@ -9,8 +9,6 @@ signal target_selector_discard_event(context)
 signal target_selector_resolved_event(context)
 
 # game-state events
-@warning_ignore("unused_signal")
-signal turn_changed_event(turn_context: Enums.Turn)
 
 #
 
