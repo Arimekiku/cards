e4f402e478f0f6a847ccaf502bab9fe10ddf11ce
Tkach Illia <tkachillia00@gmail.com>
2026-01-01 14:17:58 +0200

    Додано деякі зображення

--- scripts/effects/erase_target_effect.gd
+++ scripts/effects/erase_target_effect.gd
@@ -7,23 +7,25 @@ var running = true
 var events: EventBus = ServiceLocator.get_service(EventBus)
 
 func resolve(caster: Minion) -> void:
-	_highlight_enemies(Color.GREEN, caster)
-	events.target_selector_called_event.emit(caster)
-	
-	events.target_selector_resolved_event.connect(_stop)
+	#_highlight_enemies(Color.GREEN, caster)
+	#events.target_selector_called_event.emit(caster)
+	#
+	#events.target_selector_resolved_event.connect(_stop)
+	pass
 
 func _stop(context: Minion) -> void:
-	events.target_selector_resolved_event.disconnect(_stop)
-	
-	_highlight_enemies(Color.WHITE, context)
-	events.target_selector_discard_event.emit(context)
-	
-	running = false
-	context.take_damage(context.health)
+	#events.target_selector_resolved_event.disconnect(_stop)
+	#
+	#_highlight_enemies(Color.WHITE, context)
+	#events.target_selector_discard_event.emit(context)
+	#
+	#running = false
+	#context.take_damage(context.health)
+	pass
 
-func _highlight_enemies(color: Color, context: Control) -> void:
-	for in_zone: CardBoard in context.get_tree().get_nodes_in_group("card_zones"):
-		if in_zone.board_owner == Enums.CharacterType.PLAYER: continue
-		
-		for minion in in_zone.minions:
-			minion.modulate = color
+#func _highlight_enemies(color: Color, context: Control) -> void:
+	#for in_zone: CardBoard in context.get_tree().get_nodes_in_group("card_zones"):
+		#if in_zone.board_owner == Enums.CharacterType.PLAYER: continue
+		#
+		#for minion in in_zone.minions:
+			#minion.modulate = color
