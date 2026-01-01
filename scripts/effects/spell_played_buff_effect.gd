d7ee6b3999d93e5dcacc1cdd09e8b9c61aa9b0b4
Tkach Illia <tkachillia00@gmail.com>
2026-01-01 13:04:42 +0200

    Fixes
    Файл чомусь зник

---
+++ scripts/effects/spell_played_buff_effect.gd
@@ -0,0 +1,33 @@
+extends CardEffect
+class_name SpellPlayedBuffEffect
+
+var _callback: Callable
+var _events: EventBus
+var _minion: Minion
+
+func resolve(context) -> void:
+	print("wow")
+	if context is not Minion:
+		return
+
+	_minion = context
+	_events = ServiceLocator.get_service(EventBus)
+
+	_callback = func(card):
+		if not is_instance_valid(_minion):
+			return
+
+		if card.card_owner != _minion.minion_owner:
+			return
+
+		_minion.damage += 1
+		_minion._ui_update_damage(_minion.damage)
+
+	_events.spell_played.connect(_callback)
+
+	_minion.died_event.connect(_on_minion_died)
+
+func _on_minion_died(_m):
+	if _events and _callback:
+		if _events.spell_played.is_connected(_callback):
+			_events.spell_played.disconnect(_callback)
